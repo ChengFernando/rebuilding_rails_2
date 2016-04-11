234cdecb97390a90a848dd4e9316eee9c441cc3a
ChengFernando <fernandocheng2010@gmail.com>
2016-04-11 11:29:55 +0800

    CH1 ex1 Reloading Rulers, add debug.txt

--- rulers/lib/rulers.rb
+++ rulers/lib/rulers.rb
@@ -3,6 +3,7 @@ require "rulers/version"
 module Rulers
   class Application
     def call(env)
+      `echo debug > debug.txt`;
     [200, {'Contetn-Type' => 'text/html'}, ["Hello World!"]]
     end
   end
