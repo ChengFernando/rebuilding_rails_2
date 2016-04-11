1c49da7d4bb7065f23f17778c91001b2716b2068
ChengFernando <fernandocheng2010@gmail.com>
2016-04-11 11:23:43 +0800

    Chapter 1 build rulers Hello world

--- rulers/lib/rulers.rb
+++ rulers/lib/rulers.rb
@@ -1,5 +1,9 @@
 require "rulers/version"
 
 module Rulers
-  # Your code goes here...
+  class Application
+    def call(env)
+    [200, {'Contetn-Type' => 'text/html'}, ["Hello World!"]]
+    end
+  end
 end
